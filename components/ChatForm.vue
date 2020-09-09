f19f44bcd183a97a3ddc8a23491b948ddd3da126
ArtdSky <adpm@list.ru>
2020-09-09 23:35:42 +0600

    Vuetify design for Chat-room and Chat-input

---
+++ components/ChatForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-flex xs-12>
+    <v-text-field
+      label="Введите сообщение"
+      outlined
+      v-model="text"
+      @keydown.enter="send"
+    ></v-text-field>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: "ChatForm",
+  data: ()=> ({
+    text: ''
+  }),
+  methods: {
+    send(){
+      this.socket.emit('createMessage', {
+        text: this.text,
+        id: this.$store.state.user.id
+      }, (data)=>{
+        if(typeof data === 'string'){
+          console.error(data)
+        } else {
+          this.text = ''
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/chat.vue
+++ pages/chat.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="m in messages" :key="m.text">{{m.text}}</li>
-    </ul>
+  <div class="c-wrap">
+    <div class="c-chat">
+      <Message
+        v-for="m in messages" :key="m.text"
+        :name="m.name"
+        :text="m.text"
+        owner
+        />
+    </div>
+    <div class="c-form">
+  <ChatForm />
+    </div>
   </div>
 </template>
 
 <script>
+import Message from "../components/Message";
+import ChatForm from "../components/ChatForm";
 import { mapState } from "vuex";
 export default {
   middleware: ["chat"],
@@ -15,7 +25,36 @@ export default {
       title: `Комната ${this.user.room}`
     };
   },
+  components: {
+    Message, ChatForm
+  },
   computed: mapState(["user", "messages"])
 };
 </script>
+
+<style scoped>
+.c-wrap{
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+}
+.c-form{
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+  height: 80px;
+  background: #212121;
+}
+.c-chat{
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 80px;
+  padding: 1rem;
+  overflow-y: auto;
+}
+</style>
 
